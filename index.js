3ba7b3c74fc948999142a580d6ef3d9fda979b35
CatherineHonors <cathonors@gmail.com>
2022-04-07 11:24:29 -0400

    commit tree bc85bc676b850bdfe525823f0c4e754d6ba8e8cd

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+//const superBowlWin = record.find(element => element === "W");
+
+superbowlWin = (records) => {
+    const result = records.find(record => record.result === "W")
+        return result.year
+        
+        console.log(result);
+    }
+
